f4e6d91b8367a6af8bc6efbee6f3601886a0f323
Bill Westlake <wwestlake@lagdaemon.com>
2018-11-13 09:07:00 -0500

    Input output program

--- UserInput/Program.fs
+++ UserInput/Program.fs
@@ -4,8 +4,21 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+
+    let rec innerFunction () =
+        printfn "Enter a phrase, must be greater than 6 charactes long"
+    
+        let line = Console.ReadLine()
+
+        if line.Length > 6 then
+            printfn "Correct length: '%s'" line
+        else 
+            printfn "I said the phrase must be greater than 6 characters"
+            innerFunction ()
+
+    innerFunction ()
 
 
+    printfn "Done: press any key to exit"
     Console.ReadKey() |> ignore
     0 // return an integer exit code
