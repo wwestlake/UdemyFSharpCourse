8c440e0e096aff24c300e9cb3e459afe749b0180
Bill Westlake <wwestlake@lagdaemon.com>
2018-12-05 20:46:28 -0500

    completed lecture on Record types and intro to modules

---
+++ LagDaemon.FSharp.Utilities/Password.fs
@@ -0,0 +1,59 @@
+﻿namespace LagDaemon.FSharp.Utilities
+
+(* 
+   Password requirements
+
+   Passwords must meet the following requirements:
+        1. Be a configurable number of characters in length.
+        2. Contain no part of the users name or email address.
+        3. Is neither null nor empty.
+        4. Does not contain white space.
+        5. Contain at least:
+            a. Configurable number of uppercase characters.
+            b. Configurable number of lowercase characters.
+            c. Configurable number of digits.
+            d. Configurable number of special characters.
+
+*)
+
+module Password =
+
+    open System
+    open Result
+
+    type PasswordRules = {
+        MinLength: int;
+        MaxLength: int;
+        MinLowerCase: int;
+        MinUpperCase: int;
+        MinDigits: int;
+        MinSpecial: int;
+    }
+
+    let createRules minlen maxlen minlower minupper mindigits minspecil = {
+        MinLength = minlen;
+        MaxLength = maxlen;
+        MinLowerCase = minlower;
+        MinUpperCase = minupper;
+        MinDigits = mindigits;
+        MinSpecial = minspecil;
+    }
+
+
+
+    let passwordIsNotNull password =
+        if String.IsNullOrEmpty(password) 
+        then Failure "Password must not be null or empty"
+        else Success password
+
+    let passwordLength rules (password:string) =
+        let errorString = sprintf "Password length must be >= %d and <= %d" rules.MinLength rules.MaxLength
+
+        match password.Length < rules.MinLength, password.Length > rules.MaxLength with
+        | true, false -> Failure errorString
+        | false, true -> Failure errorString
+        | false, false -> Success password
+        | _,_ -> Failure "can't happen"
+
+
+
--- LagDaemon.FSharp.Utilities/Result.fs
+++ LagDaemon.FSharp.Utilities/Result.fs
@@ -1,5 +1,18 @@
 namespace LagDaemon.FSharp.Utilities
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module Result =
+    
+    
+    type Result<'Tsuccess, 'Tfailure> =
+        | Success of 'Tsuccess
+        | Failure of 'Tfailure
+    
+    let bind f input =
+        match input with
+        | Success s -> f s
+        | Failure f -> Failure f
+    
+    let (>>=) input func =
+        bind func input
+    
+    
